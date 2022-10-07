02db3a44da35e49ffec84681bcad85bc03a7c4b5
NoelVega126 <noeljr126@gmail.com>
2022-10-07 13:58:16 -0500

    extended next-auth session type to include username and createUsername mutation works

--- backend/src/graphql/resolvers/user.ts
+++ backend/src/graphql/resolvers/user.ts
@@ -1,10 +1,65 @@
+import { CreateUsernameResponse, GraphQLContext } from '../../util/types';
+
 const userResolvers = {
   Query: {
     searchUsers: () => {},
   },
   Mutation: {
-    createUsername: (_parent: any, args: any) => {
-      console.log(args);
+    createUsername: async (
+      _: any,
+      args: { username: string },
+      ctx: GraphQLContext
+    ): Promise<CreateUsernameResponse> => {
+      const { username } = args;
+      const { session, prisma } = ctx;
+      // console.log('HERE IS ARGS ', args);
+      console.log('HERE IS Session ', session);
+      console.log('HERE IS USER  ', session?.user);
+
+      if (!session?.user) {
+        return {
+          error: 'Not authorized',
+        };
+      }
+
+      const { id: userId } = session.user;
+
+      try {
+        /**
+         * Check that username is not taken
+         */
+        console.log('Checking if user exists');
+        const existingUser = await prisma.user.findUnique({
+          where: {
+            username,
+          },
+        });
+
+        if (existingUser) {
+          console.log('Username already taken try another');
+          return {
+            error: 'Username already taken try another',
+          };
+        }
+        /**
+         * Update User
+         */
+        console.log('USER_ID HERE ', userId);
+        const user = await prisma.user.update({
+          where: {
+            id: userId,
+          },
+          data: {
+            username,
+          },
+        });
+        return { success: true };
+      } catch (error: any) {
+        console.log('createUsername error', error);
+        return {
+          error: error.message,
+        };
+      }
     },
   },
   // Subscription: {},
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,8 +8,13 @@ import {
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import express from 'express';
 import http from 'http';
+import { getSession } from 'next-auth/react';
+import * as dotenv from 'dotenv';
+import { GraphQLContext, Session } from './util/types';
+import { PrismaClient } from '@prisma/client';
 
 async function main() {
+  dotenv.config();
   const app = express();
   const httpServer = http.createServer(app);
 
@@ -18,21 +23,41 @@ async function main() {
     resolvers,
   });
 
+  const corsOptions = {
+    origin: process.env.CLIENT_ORIGIN,
+    credentials: true,
+  };
+
+  /**
+   *  Context parameters
+   */
+  const prisma = new PrismaClient();
+  // const pubsub
+
   const server = new ApolloServer({
     schema,
     csrfPrevention: true,
     cache: 'bounded',
+    context: async ({ req, res }): Promise<GraphQLContext> => {
+      // console.log(req)
+      const session = (await getSession({ req })) as Session;
+      return { session, prisma };
+    },
     plugins: [
       ApolloServerPluginDrainHttpServer({ httpServer }),
       ApolloServerPluginLandingPageLocalDefault({ embed: true }),
     ],
   });
   await server.start();
-  server.applyMiddleware({ app });
+  server.applyMiddleware({ app, cors: corsOptions, path: '/graphql' });
   await new Promise<void>((resolve) =>
-    httpServer.listen({ port: 4000 }, resolve)
+    httpServer.listen({ port: 4001 }, resolve)
   );
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+  console.log(process.env.CLIENT_ORIGIN);
+
+  console.log(`🚀 Server ready at http://localhost:4001${server.graphqlPath}`);
 }
 
-main().catch((err) => console.log(err));
+main().catch((err) => {
+  console.log(err);
+});
