05a37ed04203aa99b6f585b73a9bbe1d0d21e31e
NoelVega126 <noeljr126@gmail.com>
2022-10-06 22:49:46 -0500

    graphql setup with resolvers and typedefs for user

--- frontend/src/components/Auth/index.tsx
+++ frontend/src/components/Auth/index.tsx
@@ -30,17 +30,19 @@ const Auth: React.FunctionComponent<IAuthProps> = ({
       <div className='flex flex-col gap-10 items-center'>
         {session ? (
           <>
-            <p className='text-3xl  tracking-wide'>Create a Username</p>
+            <p className='text-3xl  tracking-wide shadow-2xl'>
+              Create a Username
+            </p>
             <input
               placeholder='username'
               value={username}
               onChange={(event) => setUsername(event.target.value)}
-              className='w-full text-white p-2 px-3 rounded outline-none bg-transparent border-2 border-neutral-500 placeholder:tracking-wide'
+              className='w-full text-white p-2 px-3 rounded outline-none bg-transparent border-2 border-neutral-500/50 placeholder:tracking-wide hover:border-neutral-500 focus:border-neutral-500 transition-all duration-150'
             />
 
             <motion.button
               whileTap={{ scale: 1 }}
-              whileHover={{ scale: 1.025 }}
+              whileHover={{ scale: 1.009 }}
               onClick={onSubmit}
               className='text-lg  px-4 py-2 w-full rounded bg-blue-600 hover:bg-blue-700  font-semibold '
             >
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,13 +2,18 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
+import { ApolloProvider } from '@apollo/client/react';
+import { ApolloClient } from '@apollo/client/core';
+import { client } from '../graphql/apollo-client';
 // import { theme } from '../chakra/theme';
 
 function MyApp({ Component, pageProps }: AppProps<{ session: Session }>) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <ApolloProvider client={client}>
+      <SessionProvider session={pageProps.session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ApolloProvider>
   );
 }
 
